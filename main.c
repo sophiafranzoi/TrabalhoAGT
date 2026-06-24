a2c5cdfde40e4d8ef89d309416976ab35616d727
leticiabergmann <leticia.bergmann@icloud.com>
2026-06-24 10:41:41 -0300

    cadastro especifico

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include <string.h>
 
 #define MAX_LIVROS 100 //fiz com 100 só pra teste, mas podemos mudar pra menos
-#define TAM_TITULO 50
+#define TAM_TITULO 30
+#define DISTANCIA 5
 
 //  MATRIZ 1 - TITULOS (sophia) 
 char titulos[MAX_LIVROS][TAM_TITULO];
@@ -55,6 +56,10 @@ void listarTitulos() {
 
     printf("\n----Titulos de Livros-----Paginas--------- \n");
     for (int i = 0; i < totalLivros; i++) {
+        int tamanho;
+        //for (tamanho = 0; titulos[tamanho]; tamanho++);
+            //tamanho = 
+            int num_espacos = DISTANCIA + TAM_TITULO - tamanho;
         printf("%d - %s  %i\n", i, titulos[i], paginas[i]); //tentar deixar alinhado como no exemplo
     }
 }
@@ -72,6 +77,46 @@ int buscarTitulo(char titulo[]) {
 
 // FIM DA MATRIZ 1
 // (a Matriz 3 - Emprestimos, que usa buscarTitulo(), tambem pode entrar como uma terceira secao aqui, se for o caso)
+int emprestimos[MAX_LIVROS][5];
+int cadastros = 0, valido, dia_inicio, dias; 
+char emprestimo[TAM_TITULO];
+
+
+void cadastro_especifico(){
+    printf("\nDigite o titúlo que deseja cadastrar: ");
+    scanf(" %[^\n]", emprestimo);
+
+   
+    if (buscarTitulo(emprestimo) != -1) {
+        emprestimos[cadastros][1] = buscarTitulo(emprestimo);
+        emprestimos[cadastros][2] = buscarTitulo(emprestimo);
+        valido = 1;
+    }
+    else {
+        printf("Esse titulo nao esta cadastrado na biblioteca de livros\n");
+        valido = 0;
+    }
+    
+    if (valido == 1){
+
+        do{
+            printf("\nDigite o dia do início do emprestimo:\n2 - Segunda\n3 - Terca\n4 - Quarta\n5 - Quinta\n6 - Sexta\nOpcao: "); 
+            scanf("%i", &dia_inicio);
+
+            if (dia_inicio >= 2 && dia_inicio <= 6) emprestimos[cadastros][3] = dia_inicio;
+            else printf("Opcao invalida!\n");
+        } while (dia_inicio < 2 || dia_inicio > 6);
+
+        do{
+            printf("\nDigite o numero de dias para o emprestimo (até 7 dias): ");
+            scanf("%i", &dias);
+
+            if (dias >= 1 && dias <= 7) emprestimos[cadastros][4] = dias;
+            else printf("Numero de dias invalido!\n");
+        } while (dias < 1 || dias > 7);
+    }
+
+}
 
 int main() {
     int opcao;
@@ -81,6 +126,7 @@ int main() {
         printf("1 - Cadastrar titulo\n");
         printf("3 - Listar titulos\n");
         printf("4 - Buscar titulo\n");
+        printf("5 - Cadastrar emprestimo\n");
         // quando juntar com a matriz 2, adicionar aqui as opcoes dela
         // (ex: 4 - Cadastrar paginas, 5 - Listar paginas)
         printf("0 - Sair\n");
@@ -101,10 +147,13 @@ int main() {
 
             int indice = buscarTitulo(busca);
             if (indice == -1) {
-                printf("Titulo nao encontrado :( \n");
+                printf("\nTitulo nao encontrado :( \n");
             } else {
-                printf("Titulo encontrado no indice %d :)\nTitulo: %s  Paginas: %i", indice, titulos[indice], paginas[indice]);
+                printf("\nTitulo encontrado no indice %d :)\nTitulo: %s  - Paginas: %i\n", indice, titulos[indice], paginas[indice]);
             }
+        }
+        else if (opcao == 5){
+            cadastro_especifico();
         }
         else if (opcao == 0) {
             printf("Que pena, voce saiu :(\n");
